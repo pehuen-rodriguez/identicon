cd92f2ebbf10168141f35db898f9ef847a156d83
Pehuén <pehuen.rodriguez@nayra.coop>
2020-01-24 13:49:50 -0300

    Add hash input fn

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,32 @@
 defmodule Identicon do
   @moduledoc """
-  Documentation for Identicon.
+  Identicon will create a simetric pixel image based on an input string.
   """
 
   @doc """
-  Hello world.
+  Main.
 
   ## Examples
 
-      iex> Identicon.hello()
-      :world
+      iex> Identicon.main("banana")
+      will return your image
 
   """
-  def hello do
-    :world
+  def main(input) do
+    input
+    |> hash_input
+  end
+
+  @doc """
+  String hasher. Will return an MD5 hash of the string in a list form.
+
+  ## Examples
+
+      iex> Identicon.hash_input("banana")
+      [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]
+  """
+  def hash_input(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
   end
 end
